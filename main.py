8ee58f9c6c12909d1dc519a4358cccec977eecaf
Anja <anja.culjkovic@gmail.com>
2026-02-04 12:45:05 +0100

    Updated product information

--- main.py
+++ main.py
@@ -3,15 +3,10 @@ from product_manager import ProductManager
 
 m = ProductManager()
 
-p1 = Product("Phone", 850, 8)
-p2 = Product("TV", 1200, 5)
-p3 = Product("Laptop", 680, 7)
+p1 = Product("iPhone", 850, 8)
+p2 = Product("TV", 1200, 3)
+p3 = Product("Gaming Laptop", 680, 7)
 
 m.add_product(p1)
 m.add_product(p2)
 m.add_product(p3)
-
-print("List of products:")
-m.show_products()
-
-print(f"\nTotal value of products: ${m.total_value()}")
