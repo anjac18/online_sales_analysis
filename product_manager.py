489329b62dc21da15e6fabdcf3b2230a3cdf6da2
Anja <anja.culjkovic@gmail.com>
2026-02-04 12:27:50 +0100

    Added new branch add-product-removal

--- product_manager.py
+++ product_manager.py
@@ -13,3 +13,11 @@ class ProductManager:
             
     def total_value(self):
         return sum(product.price * product.quantity for product in self.products)
+    
+    def remove_product(self, product_name):
+        for product in self.products:
+            if product.name == product_name:
+                self.products.remove(product)
+                print(f"Product '{product_name}' removed.")
+                return
+        print(f"Product '{product_name}' has not been found.")
